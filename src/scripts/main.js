24991b717e87515d2b8fa75ce0ae2fd9584e9064
anneraev <47757104+anneraev@users.noreply.github.com>
2019-12-11 22:57:32 -0600

    scripts main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 import domManager from './domManager';
 import htmlBuilder from './htmlBuilder';
-import math from "./math";
+import mathematics from "./math";
 
-domManager.postToDom(htmlBuilder.elementBuilder('text','sum-test', math.sum(1,3).toString()))
+domManager.postToDom(htmlBuilder.elementBuilder('text','sum-test', mathematics.sum(1,3).toString()))
 
